1dc910b1f0bd2933d0da76dd61e2ff9b3ad0716b
uhyo <uhyo@uhy.ooo>
2023-02-16 21:15:10 +0900

    add tracing

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const sdk_trace_base_1 = require("@opentelemetry/sdk-trace-base");
+const instrumentation_1 = require("@opentelemetry/instrumentation");
+const instrumentation_2 = require("@prisma/instrumentation");
+(0, instrumentation_1.registerInstrumentations)({
+    instrumentations: [new instrumentation_2.PrismaInstrumentation()],
+});
+const provider = new sdk_trace_base_1.BasicTracerProvider({
+    sampler: new sdk_trace_base_1.AlwaysOnSampler(),
+});
+provider.addSpanProcessor(new sdk_trace_base_1.SimpleSpanProcessor(new sdk_trace_base_1.ConsoleSpanExporter()));
+provider.register({});
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 async function main() {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
-import { PrismaClient } from '@prisma/client'
+import {
+  AlwaysOnSampler,
+  BasicTracerProvider,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { PrismaInstrumentation } from "@prisma/instrumentation";
 
-const prisma = new PrismaClient()
+registerInstrumentations({
+  instrumentations: [new PrismaInstrumentation()],
+});
+
+const provider = new BasicTracerProvider({
+  sampler: new AlwaysOnSampler(),
+});
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+provider.register({});
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
@@ -9,11 +30,11 @@ async function main() {
 main()
   .then(async () => {
     const count = await prisma.user.count();
-    console.log({ count })
-    await prisma.$disconnect()
+    console.log({ count });
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
